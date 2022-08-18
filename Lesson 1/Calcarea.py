7f785c2b07b30528b0eca7aa75a860c7dbb01bbd
Dyamondude <103541236+Dyamondude@users.noreply.github.com>
2022-08-18 12:15:43 +1200

    Add files via upload

---
+++ Lesson 1/Calcarea.py
@@ -0,0 +1,16 @@
+# Author: Alex Gray
+#
+#
+
+
+#Variables
+#Valuse are in metres
+base=8
+height=10.5
+
+#statement (formula)
+(base*height)//2
+
+#Print to screen
+print('The area of the triange is', (base*height)//2, 'square metres')
+
---
+++ Lesson 1/Practice Task Var.py
@@ -0,0 +1,11 @@
+# Alex Gray
+# Variables
+
+from importlib.machinery import all_suffixes
+
+
+Name = "Alex"
+Height = "150 Centimetres"
+Address = "203 Townsend Rd"
+Telephone_Number = ""
+Allergy = "No"
