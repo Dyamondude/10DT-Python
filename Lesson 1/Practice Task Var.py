958ab0c3351738e4c3ebab5e2b483be0e5c282fd
Dyamondude <103541236+Dyamondude@users.noreply.github.com>
2022-08-24 10:36:26 +1200

    Add files via upload

--- Lesson 1/Practice Task Var.py
+++ Lesson 1/Practice Task Var.py
@@ -1,11 +1,18 @@
 # Alex Gray
-# Variables
-
-from importlib.machinery import all_suffixes
+# Practice task - variables, print, data type, 
 
 
+#Variables
 Name = "Alex"
 Height = "150 Centimetres"
-Address = "203 Townsend Rd"
-Telephone_Number = ""
-Allergy = "No"
+Address = "203 Townsend Rd" # Wellington, New Zealand
+Telephone_Number = "027 641 8414"
+Allergies = "I have no allergies"
+
+
+
+print('My name is', Name)
+print('I am', Height, 'tall.')
+print('My address is', Address)
+print('My phone number is',Telephone_Number)
+print(Allergies)
