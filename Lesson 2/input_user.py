f3be8c586f3ae4ef2ac3f87c1bc32cfee2a2250f
Dyamondude <103541236+Dyamondude@users.noreply.github.com>
2022-09-06 09:50:50 +1200

    Add files via upload

---
+++ Lesson 2/input_user.py
@@ -0,0 +1,36 @@
+# Alex Gray
+# Input - various types by user
+
+# input() function
+
+# Taking input from the user as a string
+# string = input()
+# Output
+# print(string)
+
+# Taking input from the user
+# name = input("Enter your name")
+# Output
+# print(name)
+
+# Taking input from the user
+# name = input("Enter your name: ")
+# Output
+# print("Hello,", name, ".")
+
+# Taking input from user as an integer
+# num = int(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from a user as a float
+# num = float(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from a user as a list
+# li = list(input(Enter a number: ))
+# Output
+# print(li)
