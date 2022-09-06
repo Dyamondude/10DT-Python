bdb811dc64e1d24aa5484363d0883d861a3ee736
Dyamondude <103541236+Dyamondude@users.noreply.github.com>
2022-09-07 10:42:11 +1200

    Add files via upload

---
+++ Lesson 2/Problem 1.py
@@ -0,0 +1,10 @@
+# Alex Gray
+# Problem 01
+
+#This program is designed to track and total goals scored by the scots #rd XI for each game played.
+
+match1_ST_Pats = 7
+match2_Well_College = 5
+match3_Well_High = 10
+Team_Total = match1_ST_Pats + match2_Well_College + match3_Well_High
+print("Total Goals Scored ", Team_Total)
